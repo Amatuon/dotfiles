48caff283699b4ea710167634c75f004c4918d53
Michael McDougal <michael.93.mcdougal@gmail.com>
2025-11-03 14:47:41 -0500

    Move hypr config under omarchy package

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ ln -sf ~/dotfiles/omarchy/.local/share/omarchy/logo.txt ~/.local/share/omarchy/l
 ln -sf ~/dotfiles/omarchy/.config/omarchy/branding/screensaver.txt ~/.config/omarchy/branding/screensaver.txt
 
 # Symlink hypr files
-ln -sf ~/dotfiles/hypr/.config/hypr/looknfeel.conf ~/.config/hypr/looknfeel.conf
+ln -sf ~/dotfiles/omarchy/hypr/.config/hypr/looknfeel.conf ~/.config/hypr/looknfeel.conf
 
 echo "Dotfiles installed."
