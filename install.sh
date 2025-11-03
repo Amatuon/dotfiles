bc82bc32cbd92ea4b4ebd47b0e8030ee8b473e5f
Michael McDougal <michael.93.mcdougal@gmail.com>
2025-11-03 14:46:31 -0500

    Add Hyprland looknfeel.conf for transparency settings

--- install.sh
+++ install.sh
@@ -5,4 +5,7 @@
 ln -sf ~/dotfiles/omarchy/.local/share/omarchy/logo.txt ~/.local/share/omarchy/logo.txt
 ln -sf ~/dotfiles/omarchy/.config/omarchy/branding/screensaver.txt ~/.config/omarchy/branding/screensaver.txt
 
+# Symlink hypr files
+ln -sf ~/dotfiles/hypr/.config/hypr/looknfeel.conf ~/.config/hypr/looknfeel.conf
+
 echo "Dotfiles installed."
